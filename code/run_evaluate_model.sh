b283da805d56add33a17c1a17becb7fc36887e09
Iddo Drori <idrori@cs.columbia.edu>
2022-06-10 11:13:32 -0400

    Add files via upload

---
+++ code/run_evaluate_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python evaluate_model.py --model trained_models/raft-synblobs.pth --mixed_precision
+#python evaluate_model.py --model trained_models/gma-synblobs.pth --mixed_precision
+#python evaluate_model.py --model trained_models/mrcnn-synblobs.pth
